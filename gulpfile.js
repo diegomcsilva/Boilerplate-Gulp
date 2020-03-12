b2c97c7af93e9e0faadeef46dec5cc9a9dfa1f25
diegomcsilva <diego.marcelo@tvglobo.com.br>
2020-03-12 19:11:00 -0300

    :heavy_plus_sign: New Dependency for javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
-const { series, parallel, src, dest } = require('gulp');
+const {
+  series,
+  parallel,
+  src,
+  dest
+} = require('gulp');
 const cssScss = require('gulp-sass');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+const rollup = require('gulp-better-rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 
 css_sass.compiler = require('node-sass');
- 
+
 function css_sass() {
   return src('./src/scss/*.scss')
+    .pipe(cssScss({
+      "bundleExec": true
+    }))
     .pipe(cssScss().on('error', cssScss.logError))
     .pipe(dest('./dist/css'));
 
@@ -15,9 +29,11 @@ function clean(cb) {
   cb();
 }
 
-function javascript(cb) {
-  // body omitted
-  cb();
+function javascript() {
+  return src('src/js/main.js')
+    .pipe(rollup({ plugins: [babel(), resolve(), commonjs()] }, 'umd'))
+    .pipe(uglify())
+    .pipe(dest('dist/'));
 }
 
 exports.default = series(clean, parallel(css_sass, javascript));
